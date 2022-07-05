bdda271ec1a58941cf0dce7cf7cade126fb28c1d
Lachlan Vass <lachlan.vass@superretailgroup.com>
2022-07-05 13:39:41 +1000

    CodeSbmission construct

---
+++ src/main/kotlin/CodeSubmission.kt
@@ -0,0 +1,6 @@
+class CodeSubmission(val name: String, var votes: Int) {
+
+    override fun toString(): String {
+        return "Name: $name, votes: $votes"
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
 
 fun main() {
 
+    val codeSubOne = CodeSubmission("New Video Streaming", 10)
+    val codeSubTwo = CodeSubmission("Red interface", 18)
+    val codeSubThree = CodeSubmission("GraphQL api", 89)
+
+    val bestCodeSubmissionsMap = listOf(codeSubOne, codeSubTwo, codeSubThree)
+        .sortedByDescending { it.votes }
+        .groupBy { it.votes }
+
+    println(bestCodeSubmissionsMap)
+
+    println(bestCodeSubmissionsMap[90])
+
 }
